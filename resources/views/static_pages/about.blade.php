df02019ef52604abdf97be13822226c73e6640b3
vagrant <vagrant@homestead>
2018-11-02 06:14:55 +0000

    Finish static pages

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', 'Sameple-关于')
+@section('content')
+    <h1>关于页</h1>
+@endsection
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', 'Sameple-主页')
+@section('content')
+    <h1>主页</h1>
+@endsection
